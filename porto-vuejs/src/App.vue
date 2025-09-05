8ddf7ebade4f9c54d18494a4d86878895c1732e5
mfrakhman <rrrfajar1@gmail.com>
2025-09-05 20:18:08 +0700

    first commit

---
+++ porto-vuejs/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <section>
+    <header>
+      <h2>Hello Friends</h2>
+    </header>
+    <ul>
+      <!-- <friend-contact
+        friendName="Fajar Rakhman"
+        phone="0821XXXX"
+        emailAddress="rrrfajar1@gmail.com"
+      ></friend-contact>
+      <friend-contact
+        friendName="Rizky Ramadhan"
+        phone="082177343680"
+        emailAddress="ramarizdev@gmail.com"
+      ></friend-contact>
+      <friend-contact
+        friendName=" Ichvandi Oktamaulana"
+        phone="082177343681"
+        emailAddress="vandoc@gmail.com"
+      ></friend-contact> -->
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :friend-name="friend.name"
+        :phone="friend.phoneNumber"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+      />
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "fajar",
+          name: "Fajar Rakhman",
+          phoneNumber: "082177343679",
+          email: "rrrfajar1@gmail.com",
+          isFavorite: false,
+        },
+        {
+          id: "rizky",
+          name: "Rizky Ramadhan",
+          phoneNumber: "082177343680",
+          email: "ramarizdev@gmail.com",
+          //   isFavorite: "0",
+        },
+        {
+          id: "kovan",
+          name: "Ichvandi Oktamaulana",
+          phoneNumber: "082177343681",
+          email: "vandoc@gmail.com",
+          //   isFavorite: "0",
+        },
+        {
+          id: "Isan",
+          name: "Achmad Ichsan",
+          phoneNumber: "082177343682",
+          email: "isanon@gmail.com",
+          //   isFavorite: "0",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
---
+++ porto-vuejs/src/components/FriendContact.vue
@@ -0,0 +1,68 @@
+<template>
+  <li>
+    <h2>
+      {{ friendName }} {{ friendIsFavorite === true ? "(Favorite)" : "" }}
+    </h2>
+    <button @click="toggleDetails">Show Details</button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ phone }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  /*Simple version of props without validation
+    props: ["friendName", "phone", "emailAddress", "isFavorite"],
+    */
+  //   Detailed version of props with validation
+  props: {
+    friendName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: true,
+      default: false,
+      //   validator: value => {
+      //     return value === "0" || value === "1";
+      //   },
+    },
+  },
+
+  data() {
+    return {
+      detailsAreVisible: false,
+      //   friend: {
+      //     id: "fajar",
+      //     name: "Fajar Rakhman",
+      //     phoneNumber: "082177343679",
+      //     email: "rrrfajar1@gmail.com",
+      //   },
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      if (this.friendIsFavorite === true) {
+        this.friendIsFavorite = false;
+      } else {
+        this.friendIsFavorite = true;
+      }
+    },
+  },
+};
+</script>
