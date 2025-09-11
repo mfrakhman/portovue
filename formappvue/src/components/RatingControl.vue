6acc6ef280f3563f081a7c93b41ed950204d01f7
mfrakhman <rrrfajar1@gmail.com>
2025-09-11 16:43:33 +0700

    form in vue

---
+++ formappvue/src/components/RatingControl.vue
@@ -0,0 +1,69 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'excelent' }">
+      <button type="button" @click="activate('excelent')">Excelent</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      //   activeOption: this.modelValue,
+    };
+  },
+  computed: {
+    // activeOption() {
+    //   return this.modelValue;
+    // },
+  },
+  methods: {
+    activate(option) {
+      this.activeOption = option;
+      this.$emit("update:modelValue", option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  background-color: green;
+}
+
+.active button {
+  color: white;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
