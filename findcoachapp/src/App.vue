ef712c7c5ad8bc83e9bb0c2dd4abe40544c7da18
mfrakhman <rrrfajar1@gmail.com>
2025-09-18 21:46:37 +0700

    find couch app

---
+++ findcoachapp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap");
+
+body {
+  margin: 0;
+}
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
